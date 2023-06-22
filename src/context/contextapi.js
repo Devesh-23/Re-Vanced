f55383d697fea1865044b3b9ca06dd47dfb5e6cc
Devesh-23 <deveshpachauri23@gmail.com>
2023-06-22 11:31:20 +0530

    api fethcing

---
+++ src/context/contextapi.js
@@ -0,0 +1,41 @@
+import React, { createContext, useState, useEffect } from "react";
+
+import { fetchDataFromApi } from "../utils/api";
+export const Context = createContext();
+
+export const AppContext = (props) => {
+    const [loading, setLoading] = useState(false);
+    const [searchResults, setSearchResults] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState("New");
+    const [mobileMenu, setMobileMenu] = useState(false);
+
+    useEffect(() => {
+        fetchSelectedCategoryData(selectedCategory);
+    }, [selectedCategory]);
+
+   // here we area getting the data from the api by destructing the contents
+    const fetchSelectedCategoryData = (query) => {
+        setLoading(true);
+        fetchDataFromApi(`search/?q=${query}`).then(({contents} ) => {
+            console.log(contents);
+            setSearchResults(contents);
+            setLoading(false);
+        });
+    };
+
+    return (
+        <Context.Provider
+            value={{
+                loading,
+                setLoading,
+                searchResults,
+                selectedCategory,
+                setSelectedCategory,
+                mobileMenu,
+                setMobileMenu,
+            }}
+        >
+            {props.children}
+        </Context.Provider>
+    );
+};
